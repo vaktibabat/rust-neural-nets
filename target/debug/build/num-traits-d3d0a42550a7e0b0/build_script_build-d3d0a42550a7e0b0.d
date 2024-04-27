17a4d5e9c668d31ff3c6062de53f4ba382b9101d
Yoray Herzberg <vaktibabat@yoray-dell.vaktibabat.com>
2024-04-27 11:28:42 +0300

    Remove Cargo.lock file

---
+++ target/debug/build/num-traits-d3d0a42550a7e0b0/build_script_build-d3d0a42550a7e0b0.d
@@ -0,0 +1,5 @@
+/home/vaktibabat/Projects/rust-neural-nets/target/debug/build/num-traits-d3d0a42550a7e0b0/build_script_build-d3d0a42550a7e0b0: /home/vaktibabat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.18/build.rs
+
+/home/vaktibabat/Projects/rust-neural-nets/target/debug/build/num-traits-d3d0a42550a7e0b0/build_script_build-d3d0a42550a7e0b0.d: /home/vaktibabat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.18/build.rs
+
+/home/vaktibabat/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.18/build.rs:
---
+++ target/debug/deps/rust_neuralnet-047c3c986b5959f1.d
@@ -0,0 +1,5 @@
+/home/vaktibabat/Projects/rust-neural-nets/target/debug/deps/librust_neuralnet-047c3c986b5959f1.rmeta: src/main.rs
+
+/home/vaktibabat/Projects/rust-neural-nets/target/debug/deps/rust_neuralnet-047c3c986b5959f1.d: src/main.rs
+
+src/main.rs:
